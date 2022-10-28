0056879fc29124644d28083c9adfea390bac11ea
Ho Yun <115262503+HYC0819@users.noreply.github.com>
2022-10-28 18:27:16 +0800

    Update index.js

--- js/index.js
+++ js/index.js
@@ -1,10 +1,9 @@
-var coords = [25.03,121.52]
+var coords = [25.04,121.535]
 
-var map = L.map('map').setView(coords, 12);
+var map = L.map('map').setView(coords, 14);
 
 L.tileLayer('http://{s}.google.com/vt/lyrs=s,h&x={x}&y={y}&z={z}', {
-    subdomains:['mt0','mt1','mt2','mt3'],
-    attribution: '&copy; <a href="https://www.openstreetmap.org/copyright">OpenStreetMap</a> contributors'
+    subdomains:['mt0','mt1','mt2','mt3']
 }).addTo(map);
 
 var negativeCircle = L.circle([25.024943, 121.543451], {
@@ -21,4 +20,52 @@ var positiveCircle = L.circle([25.041, 121.571116], {
     radius: 1000
 }).addTo(map);
 
-positiveCircle.bindPopup("Urban growth improved the economy in Taipei");
+positiveCircle.bindPopup("<p><p style='color: lime;' >Opportunities</p> of Urban Growth</p>", {
+    minWidth : 350
+});
+
+negativeCircle.bindPopup("<p><p style='color: red;' >Challenges</p> of Urban Growth</p>", {
+    minWidth : 320
+});
+
+positiveCircle.on('mouseover',function(ev) {
+    ev.target.openPopup();
+});
+
+negativeCircle.on('mouseover',function(ev) {
+    ev.target.openPopup();
+});
+
+function readTextFile(file, callback) {
+    var rawFile = new XMLHttpRequest();
+    rawFile.overrideMimeType("application/json");
+    rawFile.open("GET", file, true);
+    rawFile.onreadystatechange = function() {
+        if (rawFile.readyState === 4 && rawFile.status == "200") {
+            callback(rawFile.responseText);
+        }
+    }
+    rawFile.send(null);
+}
+
+var animation = anime({
+    targets: '.wiper',
+    translateX: -window.innerWidth,
+    translateY: -window.innerHeight,
+    delay: function (el, i) { return i * 100; },
+    duration: 500,
+    direction: 'alternate',
+    loop: false,
+    autoplay: false,
+    easing: 'easeInOutQuad',
+    complete: function(anim) {
+        document.body.style.background = "rgb(56,72,63)"
+        setTimeout(() => {
+            document.location.href = "challenge.html"
+        }, 500)
+    }
+});
+
+negativeCircle.on("click", function(ev) {
+    animation.play();
+})
